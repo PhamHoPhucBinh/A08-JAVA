56a5b705759dd4cf7f87335bcb389301039d5e31
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2022-11-18 19:28:08 +0700

    ipd

--- src/_03_Array_list/exercise/maximum_in_array/maximum_2d_array.java
+++ src/_03_Array_list/exercise/maximum_in_array/maximum_2d_array.java
@@ -1,4 +1,4 @@
-package _03_Array_list.exercise.minimum_in_array;
+package _03_Array_list.exercise.maximum_in_array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
---
+++ src/_03_Array_list/exercise/minimum_in_array/minimum_2d_array.java
@@ -0,0 +1,32 @@
+package _03_Array_list.exercise.minimum_in_array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class minimum_2d_array {
+    public static void main(String[] args) {
+
+        ArrayList<Integer[]> table = new ArrayList<>();
+        Integer[] a = {1,3,5};
+        Integer[] b = {10,30,50};
+        table.add(a);
+        table.add(b);
+
+        table.forEach(e -> {
+            System.out.println(Arrays.toString(e));
+        });
+
+//        int min = Integer.MAX_VALUE;
+        int min = table.get(0)[0];
+        for (int i = 0; i < table.size(); i++) {
+            for (int j = 0; j < table.get(i).length; j++) {
+                int t = table.get(i)[j];
+                if(t < min){
+                    min = t;
+                }
+            }
+        }
+
+        System.out.println("Min: " + min);
+    }
+}
