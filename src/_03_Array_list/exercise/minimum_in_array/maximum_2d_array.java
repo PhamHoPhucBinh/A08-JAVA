215bdf32d9e279b3e3e8a046326f24fb8bd752ba
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2022-11-18 19:06:54 +0700

    upd

---
+++ src/_03_Array_list/exercise/minimum_in_array/maximum_2d_array.java
@@ -0,0 +1,33 @@
+package _03_Array_list.exercise.minimum_in_array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class maximum_2d_array {
+    public static void main(String[] args) {
+
+        ArrayList<Integer[]> table = new ArrayList<>();
+        Integer[] a = {1,3,5};
+        Integer[] b = {10,30,50};
+        table.add(a);
+        table.add(b);
+
+        table.forEach(e -> {
+            System.out.println(Arrays.toString(e));
+        });
+
+
+        int max = 0;
+        for (int i = 0; i < table.size(); i++) {
+            for (int j = 0; j < table.get(i).length; j++) {
+                int t = table.get(i)[j];
+               if(t > max){
+                   max = t;
+               }
+            }
+        }
+
+        System.out.println("Max: " + max);
+    }
+}
+
