6becb1517b5d88d0599b841db74ff9b798677a41
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2022-11-28 14:58:27 +0700

    4

---
+++ src/_04_AccessModifier_StaticMethod/excercies/quadratic_equation/main.java
@@ -0,0 +1,27 @@
+package _04_AccessModifier_StaticMethod.excercies.quadratic_equation;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a, b, c;
+        System.out.println("input a");
+        a = scanner.nextDouble();
+        System.out.println("input b");
+        b = scanner.nextDouble();
+        System.out.println("input c");
+        c = scanner.nextDouble();
+        quadratic_equation f = new quadratic_equation(a,b,c);
+        if (f.getDiscriminant()>0){
+            System.out.println("The equation has two solutions");
+            System.out.println("nghiem 1 : " +f.getRoot1(f.getDiscriminant()));
+            System.out.println("nghiem 2 : " +f.getRoot1(f.getDiscriminant()));
+        } else if (f.getDiscriminant()==0) {
+            System.out.println("equation has double solution");
+            System.out.println("double solutions is: "+ f.getRoot1(f.getDiscriminant()));
+        }else{
+            System.out.println("no solution");
+        }
+    }
+}
---
+++ src/_04_AccessModifier_StaticMethod/excercies/quadratic_equation/quadratic_equation.java
@@ -0,0 +1,49 @@
+package _04_AccessModifier_StaticMethod.excercies.quadratic_equation;
+
+import java.util.Scanner;
+
+public class quadratic_equation {
+    private double a, b, c;
+
+    public quadratic_equation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getDiscriminant() {
+        if (a != 0) {
+            return ((Math.pow(b, 2)) - 4 * a * c);
+        } else {
+            return -1;
+        }
+    }
+
+    public double getRoot1(double delta) {
+        if (delta < 0) {
+            return 0;
+        } else {
+            return (-1 * b + Math.sqrt(delta)) / 2 * a;
+        }
+    }
+
+    public double getRoot2(double delta) {
+        if (delta < 0) {
+            return 0;
+        } else {
+            return (-1 * b + Math.sqrt(delta)) / 2 * a;
+        }
+    }
+}
