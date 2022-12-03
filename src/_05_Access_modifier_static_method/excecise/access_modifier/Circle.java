21ce9df16e0ed8eb91f323a298b22ceb4a0cb343
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2022-12-03 11:43:27 +0700

    b

---
+++ src/_05_Access_modifier_static_method/excecise/access_modifier/Circle.java
@@ -0,0 +1,8 @@
+package _05_Access_modifier_static_method.excecise.access_modifier;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+    
+
+}
--- src/_05_Access_modifier_static_method/pratice/Static_method/TestStaticMethod.java
+++ src/_05_Access_modifier_static_method/pratice/Static_method/TestStaticMethod.java
@@ -2,6 +2,7 @@ package _05_Access_modifier_static_method.pratice.Static_method;
 
 public class TestStaticMethod{
     public static void main(String[] args) {
+        Student.change();
         Student s1 = new Student(1,"a");
         Student s2 = new Student(2,"b");
         Student s3 = new Student(3,"c");
