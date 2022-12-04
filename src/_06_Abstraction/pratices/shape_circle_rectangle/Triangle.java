7ebeb40ff1dd0d58d6876c3309c239c2e6d846b1
PhamHoPhucBinh <hophucbinh2396@gmail.com>
2022-12-04 17:03:56 +0700

    up

---
+++ src/_06_Abstraction/pratices/shape_circle_rectangle/Triangle.java
@@ -0,0 +1,46 @@
+package _06_Abstraction.pratices.shape_circle_rectangle;
+
+public class Triangle extends Shape {
+    private double side1 = 1.0;
+    private double side2 = 1.0;
+    private double side3 = 1.0;
+
+    public Triangle(){};
+    public Triangle(double side1,double side2,double side3){
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+    public double getParameter(){
+        return this.side1+this.side2+this.side3;
+    }
+    public double getArea(){
+        double p = this.getParameter();
+        return Math.sqrt(p*(p-this.getSide1())*(p-this.getSide2())*
+                (p-this.getSide3()));
+    }
+}
---
+++ src/_06_Abstraction/pratices/shape_circle_rectangle/TriangleTest.java
@@ -0,0 +1,8 @@
+package _06_Abstraction.pratices.shape_circle_rectangle;
+
+public class TriangleTest {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(6,6,6);
+        System.out.println(triangle.getParameter());
+    }
+}
