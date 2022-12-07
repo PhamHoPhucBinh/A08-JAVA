31ed239219867840819eb6fb4f25f589230c0ff6
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2022-12-07 18:59:04 +0700

    compa

--- src/_06_Inheritence/pratices/shape_circle_rectangle/ComparableCircleTest.java
+++ src/_06_Inheritence/pratices/shape_circle_rectangle/ComparableCircleTest.java
@@ -1,19 +1,21 @@
 package _06_Inheritence.pratices.shape_circle_rectangle;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ComparableCircleTest {
     public static void main(String[] args) {
-        ComparableCircle[] circles = new ComparableCircle[3];
+        ComparableCircle[] circles = new ComparableCircle[4];
         circles[0] = new ComparableCircle(3.6);
         circles[1] = new ComparableCircle();
         circles[2] = new ComparableCircle(3, "blue", true);
+        circles[3] = new ComparableCircle(3.1, "red", true);
         System.out.println("Pre-sorted:");
         for (ComparableCircle circle : circles) {
             System.out.println(circle);
         }
 
-        Arrays.sort(circles);
+        Arrays.sort(circles, Comparator.comparingDouble(ComparableCircle::getRadius) );
 
         System.out.println("After-sorted:");
         for (ComparableCircle circle : circles) {
