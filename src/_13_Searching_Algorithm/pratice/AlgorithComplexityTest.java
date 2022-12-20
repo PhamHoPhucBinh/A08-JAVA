8d23bf293ea4d6a32414f766406beadb1285cf90
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2022-12-20 10:00:07 +0700

    algo

--- src/_13_Searching_Algorithm/pratice/AlgorithComplexityTest.java
+++ src/_13_Searching_Algorithm/pratice/AlgorithComplexityTest.java
@@ -10,17 +10,27 @@ public class AlgorithComplexityTest {
 
 
         int[] frequentChar = new int[255];
+        System.out.println("1");
         for (int i = 0; i < inputString.length(); i++) {
+            System.out.println("2");
         int ascii = (int) inputString.charAt(i);
+            System.out.println("3");
         frequentChar[ascii] +=1;
+            System.out.println("4");
         }
 
         int max = 0;
+        System.out.println("5");
         char character = (char) 255;
+        System.out.println("6");
         for (int j = 0;j<255;j++){
+            System.out.println("7");
             if (frequentChar[j]>max){
+                System.out.println("8");
                 max = frequentChar[j];
+                System.out.println("9");
                 character = (char) j;
+                System.out.println("10");
             }
         }
         System.out.println(character +": "+ max + " time");
